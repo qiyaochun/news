ac4f02c06f79de78d04bc2d040564cf9b5dae814
qiyaochun <554112588@qq.com>
2018-06-09 17:48:23 +0800

    test

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hellow():
+    return  render_template('m_01.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
